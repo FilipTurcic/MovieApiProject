0576b12dfd07c0655694fde9ab5a7710a02d4598
Damir Filip Turcic <132576582+FilipTurcic@users.noreply.github.com>
2024-01-09 10:54:22 -0500

    Add files via upload

---
+++ MovieStoreAPI/MovieStoreAPI/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Year is required")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        // Add other properties as needed (e.g., Director, Duration, Rating, etc.)
+    }
+}
